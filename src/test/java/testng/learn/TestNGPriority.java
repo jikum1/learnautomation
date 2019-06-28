ab1960b39ec545264e751f3558d26b075603d25e
jitukum <jitendrakumar.nitmz@gmail.com>
2019-06-28 08:29:32 +0530

    Squash the merge changes

--- src/test/java/testng/learn/TestNGPriority.java
+++ src/test/java/testng/learn/TestNGPriority.java
@@ -1,4 +1,22 @@
 package testng.learn;
 
+import org.testng.annotations.Test;
+
 public class TestNGPriority {
+
+    @Test
+    public void method1(){
+
+    }
+    
+    @Test
+    public void method2(){
+        System.out.println("Inside method 2");
+    }
+
+    
+    @Test
+    public void method3(){
+        System.out.println("Inside method 3");
+    }
 }
