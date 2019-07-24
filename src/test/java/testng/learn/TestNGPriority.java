65c59ad97a28d7ba6cd2451c3da5aca40071675a
jitukum <jitendrakumar.nitmz@gmail.com>
2019-07-24 22:32:49 +0530

    Added file to run Jenkins file

--- src/test/java/testng/learn/TestNGPriority.java
+++ src/test/java/testng/learn/TestNGPriority.java
@@ -6,17 +6,17 @@ public class TestNGPriority {
 
     @Test
     public void method1(){
-        System.out.println("Inside method 1");
+        System.out.println("**********Inside method 1**********");
     }
 
     @Test(priority = 0)
     public void method2(){
-        System.out.println("Inside method 2");
+        System.out.println("***********Inside method 2********");
     }
 
 
     @Test(priority = 1)
     public void method3(){
-        System.out.println("Inside method 3");
+        System.out.println("*************Inside method 3**********");
     }
 }
